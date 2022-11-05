ac6495ccc1d1d2f87d6e7924c205033c457f6354
Manoela Cirilo <manoelacirilo04@gmail.com>
2022-11-05 12:23:17 -0300

    corrigir nomenclatura

---
+++ README.md
@@ -0,0 +1,25 @@
+# Pokemon App
+
+Sistema Django para gerenciar Pokemon .
+Funcionalidades de insirir, listar, editar e excluir Pokemon.
+Login de usuário.
+
+# Instalação
+
+## Instalar dependências
+
+```bash
+pip install -r requirements.txt
+```
+
+## Criar .env
+
+```bash
+cp env.example .env
+```
+
+## Rodar servidor
+
+```bash
+python manage.py runserver
+```
--- pokemonAppAdmin/wsgi.py
+++ pokemonAppAdmin/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for projectdesafoReactAdmin project.
+WSGI config for pokemonAppAdmin project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projectdesafoReactAdmin.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pokemonAppAdmin.settings')
 
 application = get_wsgi_application()
